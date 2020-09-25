3054231442fbb098711c5ae7d057aea90600aa30
Ihor986 <235689ks@gmail.com>
2020-09-25 12:23:12 +0300

    fixed structural deficiencies

--- dz1/index.php
+++ dz1/index.php
@@ -6,8 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-
-    <?php
+<?php
     $maxMonthlyPayment = 5000;
     $creditAmount = 34499;
   // масив банків  
@@ -31,47 +30,40 @@
             "accountFee" => 6666
             ]
         ];
-        // повертаємо значення загальної вартості товару з урахуванням кредиту від банку(можливо обрахувати загальний термін)
+        // повертаємо значення загальної вартості товару з урахуванням кредиту від банку та загальний термін
     function totalCreditAmount($bankName, $creditAmount, $maxMonthlyPayment){
      $cost = $creditAmount + $bankName[accountFee];
-    //  $creditTermValue = 1;
-     $i = $cost - $maxMonthlyPayment;
+     $creditTermValue = 1;
+     $outstandingAmount = $cost - $maxMonthlyPayment;
      $totalCreditAmount = $cost;
-     while ($i > 0) {
-             $interest = round($i*$bankName[interest], 2);
+     while ($outstandingAmount > 0) {
+             $interest = round($outstandingAmount*$bankName[interest], 2);
              $monthCommission = $bankName[monthCommission];
-           $i = $i + $interest + $monthCommission - $maxMonthlyPayment;
-        //    $creditTermValue++;
+           $outstandingAmount = $outstandingAmount + $interest + $monthCommission - $maxMonthlyPayment;
+           $creditTermValue++;
            $totalCreditAmount = $totalCreditAmount + $interest + $monthCommission;
          } 
-     echo "Загальна вартість товару з урахуванням кредиту від банку \"$bankName[name]\" становить:  $totalCreditAmount грн!<br/>";
-    //  echo "Кредит буде погашено за $creditTermValue міс.<hr><br/>";
-     return $totalCreditAmount;
-    }
-    // повертаємо термін погашення кредиту(можливо обрахувати загальну вартість товару з урахуванням кредиту від банку)
-    function creditTerm($bankName, $creditAmount, $maxMonthlyPayment){
-     $cost = $creditAmount + $bankName[accountFee];
-     $creditTermValue = 1;
-     $i = $cost - $maxMonthlyPayment;
-    //  $totalCreditAmount = $cost;
-      while ($i > 0) {
-             $interest = round($i*$bankName[interest], 2);
-             $monthCommission = $bankName[monthCommission];
-           $i = $i + $interest + $monthCommission - $maxMonthlyPayment;
-           $creditTermValue++;
-        //    $totalCreditAmount = $totalCreditAmount + $interest + $monthCommission;
-         } 
-         //  echo "Загальна вартість товару з урахуванням кредиту від банку \"$bankName[name]\" становить:  $totalCreditAmount грн!<br/>";
-     echo "Кредит буде погашено за $creditTermValue міс.<hr><br/>";
-     return $creditTerm;
-    }
-    // присвоюємо значення вартості та термінів змінним
-    $totalCreditAmountHomoCredit = totalCreditAmount($banks[homoCredit], $creditAmount, $maxMonthlyPayment);
-    $totalCreditTermHomoCredit = creditTerm($banks[homoCredit], $creditAmount, $maxMonthlyPayment);
-    $totalCreditAmountSoftbank = totalCreditAmount($banks[softbank], $creditAmount, $maxMonthlyPayment);
-    $totalCreditTermSoftbank = creditTerm($banks[softbank], $creditAmount, $maxMonthlyPayment);
-    $totalCreditAmountStrawberryBank = totalCreditAmount($banks[strawberryBank], $creditAmount, $maxMonthlyPayment);
-    $totalCreditTermStrawberryBank = creditTerm($banks[strawberryBank], $creditAmount, $maxMonthlyPayment);
+     $totalCreditAmountAndTerm = [
+         "amount" => $totalCreditAmount,
+         "term" => $creditTermValue
+        ];
+    //  echo "Загальна вартість товару з урахуванням кредиту від банку $bankName[name] становить:  $totalCreditAmountAndTerm[amount] грн!<br/>";
+    //  echo "Кредит буде погашено за $totalCreditAmountAndTerm[term] міс.<hr><br/>";   
+     return $totalCreditAmountAndTerm;
+    };
+// виводимо результат
+
+
+
+    // totalCreditAmount($banks[homoCredit], $creditAmount, $maxMonthlyPayment);
+    
+    // // присвоюємо значення вартості та термінів змінним
+    // $totalCreditAmountAndTermHomoCredit = totalCreditAmount($banks[homoCredit], $creditAmount, $maxMonthlyPayment);
+    // // $totalCreditTermHomoCredit = creditTerm($banks[homoCredit], $creditAmount, $maxMonthlyPayment);
+    // $totalCreditAmountAndTermSoftbank = totalCreditAmount($banks[softbank], $creditAmount, $maxMonthlyPayment);
+    // // $totalCreditTermSoftbank = creditTerm($banks[softbank], $creditAmount, $maxMonthlyPayment);
+    // $totalCreditAmountAndTermStrawberryBank = totalCreditAmount($banks[strawberryBank], $creditAmount, $maxMonthlyPayment);
+    // // $totalCreditTermStrawberryBank = creditTerm($banks[strawberryBank], $creditAmount, $maxMonthlyPayment);
 ?>
     
 </body>
