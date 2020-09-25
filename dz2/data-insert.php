edf50bbea834e7fca82f1251b2158dcb20458fc7
Ihor986 <235689ks@gmail.com>
2020-09-25 11:40:48 +0300

    message generation is made a separate function and other improvements

--- dz2/data-insert.php
+++ dz2/data-insert.php
@@ -5,6 +5,7 @@ try {
     } catch (Exception $ex) {
       $availableBanknotes = [];
     }
+$usedBanknotes = [];  
 // рахуємо загальну суму коштів    
 function findMoneySumm($dataArray){
 $sum = array_reduce(array_keys($dataArray), function($carry, $key) use ($dataArray) {
@@ -13,54 +14,52 @@ $sum = array_reduce(array_keys($dataArray), function($carry, $key) use ($dataArr
 }, 0);
 return $sum;
 } ;
-$findMoneySumm = findMoneySumm($availableBanknotes);
-$desiredAmount = htmlspecialchars($_POST['amount']);
-$balanceAmount = $desiredAmount;
-$usedBanknotes = [];
-foreach($availableBanknotes as $key => $value){
-    $used = floor($balanceAmount/$key);
-   
-    $availableBanknotes[$key] = $availableBanknotes[$key] - $used;
-    $balanceAmountMod = 0;
-    if ($availableBanknotes[$key] < 0){
-      $balanceAmountMod = abs ( $availableBanknotes[$key] )*$key;
-      $used = $used + $availableBanknotes[$key];
-      $availableBanknotes[$key] = 0; 
-     
-    };
-    $balanceAmount = $balanceAmount % $key + $balanceAmountMod;
-    
-    $usedBanknotes[$key] = $used;
-     if ($usedBanknotes[$key]) {
-       
-        $message = $message.$usedBanknotes[$key]."*".$key." ";
-   };
-};
-// виведення помилки
-function echoError ($errorMessage){
-    echo "<div>$errorMessage</div>";
-    echo "<div><button><a href=\"index.php\">повернення до форми вводу</a></button></div>";
-    exit();
-};
 // перевіряємо наявність виключень
 function searchError ($desiredAmount,$findMoneySumm, $usedBanknotes){
 if ($desiredAmount>$findMoneySumm){
   $errorMessage = "Недостатньо коштів";
 } else if (!$desiredAmount){
   $errorMessage = "Невірно вказана сума (нуль неможливо видати)";
-} else if (gettype($desiredAmount/5)!==integer){
+} else if ($desiredAmount%5!==0){
   $errorMessage = "Невірно вказана сума (не кратна 5, неможливо видати)";
 } else if (findMoneySumm ($usedBanknotes)<$desiredAmount){
   $errorMessage = "Неможливо видати (недостатньо дрібних купюр)";
- } else {
-  return false;
-}
-echoError ($errorMessage);
+ };
+ return $errorMessage;
 };
-searchError ($desiredAmount, $findMoneySumm, $usedBanknotes);
+// формуємо повідомлення для відображення числа виданих купюр
+function messageGenerator ($usedBanknotes) {
+  foreach($usedBanknotes as $key => $value){
+    if ($usedBanknotes[$key]) {
+      $message = $message.$usedBanknotes[$key]."*".$key." ";
+    }
+   }
+   return $message;
+};
+$findMoneySumm = findMoneySumm($availableBanknotes);
+$desiredAmount = htmlspecialchars($_POST['amount']);
+$balanceAmount = $desiredAmount;
+foreach($availableBanknotes as $key => $value){
+    $used = floor($balanceAmount/$key);
+    $availableBanknotes[$key] = $availableBanknotes[$key] - $used;
+    $balanceAmountMod = 0;
+    if ($availableBanknotes[$key] < 0){
+      $balanceAmountMod = abs ( $availableBanknotes[$key] )*$key;
+      $used = $used + $availableBanknotes[$key];
+      $availableBanknotes[$key] = 0; 
+     };
+    $balanceAmount = $balanceAmount % $key + $balanceAmountMod;
+    $usedBanknotes[$key] = $used;
+    };
+$message = messageGenerator ($usedBanknotes);   
+$errorMessage = searchError ($desiredAmount, $findMoneySumm, $usedBanknotes);
+if ($errorMessage){
+echo "<div>$errorMessage</div>";
+ } else {
 echo "Сума: $desiredAmount </br>";
 echo "Число купюр: $message";
-echo "<div><button><a href=\"index.php\">повернення до форми вводу</a></button></div>";
 $newDataSrt = json_encode($availableBanknotes, JSON_PRETTY_PRINT);
 $result = file_put_contents('data.json', $newDataSrt);
+ };
+echo "<div><button><a href=\"index.php\">повернення до форми вводу</a></button></div>";
 ?>
