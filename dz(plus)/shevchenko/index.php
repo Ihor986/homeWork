51625d18f414c0d8acf98a8cc1ed1309ffc4e0d2
Ihor986 <235689ks@gmail.com>
2020-09-29 12:09:33 +0300

    cosmetic

--- dz(plus)/shevchenko/index.php
+++ dz(plus)/shevchenko/index.php
@@ -4,37 +4,36 @@ try {
     } catch (Exception $ex) {
       $dataArr = [];
     }
-// дізнаємося довжину найдовшого рядка    
-$countLine = 0;
-foreach ($dataArr as $line) {
-    $countL = mb_strlen($line);
-if ($countL > $countLine){
-     $countLine = $countL;
- };
-};  
-// дізнаємося кількість рядеків
-$countArr = count ($dataArr);
-// виводимо результат
 function printLine($dataArr, $countLine){
-$countArr = count ($dataArr);
-for ($i=0; $i<$countLine; $i++) {
-    $line ="";
-   for ($q=0; $q<$countArr; $q++){
-     if ($dataArr[$q] == $dataArr[0]){
-       $arrDataStr = preg_split('/(?<!^)(?!$)/u', $dataArr[$q]);
-       array_splice($arrDataStr, 0, 1);
-     } else {
-       $arrDataStr = preg_split('/(?<!^)(?!$)/u', $dataArr[$q]);
-     };
-     array_pop($arrDataStr);
-     if (!$arrDataStr[$i]){
-         $arrDataStr[$i] = " ";
-     };
-     $line = $line.$arrDataStr[$i]."      ";
-     
- }
-echo "<pre>$line </br></pre>";
-};
+  // дізнаємося довжину рядка
+  $countLine = 0;
+  foreach ($dataArr as $line) {
+      $countL = mb_strlen($line);
+  if ($countL > $countLine){
+      $countLine = $countL;
+  };
+  };  
+  // дізнаємося кількістьрядків
+  $countArr = count ($dataArr);
+  // формуємо та виводимо результат
+  for ($i=0; $i<$countLine; $i++) {
+      $line ="";
+    for ($q=0; $q<$countArr; $q++){
+      if ($dataArr[$q] == $dataArr[0]){
+        $arrDataStr = preg_split('/(?<!^)(?!$)/u', $dataArr[$q]);
+        array_splice($arrDataStr, 0, 1);
+      } else {
+        $arrDataStr = preg_split('/(?<!^)(?!$)/u', $dataArr[$q]);
+      };
+      array_pop($arrDataStr);
+      if (!$arrDataStr[$i]){
+          $arrDataStr[$i] = " ";
+      };
+      $line = $line.$arrDataStr[$i]."      ";
+      
+  }
+  echo "<pre>$line </br></pre>";
+  };
 }
 printLine($dataArr, $countLine);
 ?>
